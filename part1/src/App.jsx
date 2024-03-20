1999686be59dbb1ef95eb67d2b8237c6ee2ecd7b
Daniel <danielnavatin@gmail.com>
2024-03-20 17:45:14 +0100

    Add part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  console.log('Hello')
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
