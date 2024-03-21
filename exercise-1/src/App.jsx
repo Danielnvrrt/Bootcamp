1717a160153d2f134075932b4f02469038471d4a
Daniel <danielnavatin@gmail.com>
2024-03-21 18:04:53 +0100

    Exercise 1.4

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -30,7 +30,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.total.reduce((a,b) => a+b, 0)}</p>
     </>
   )
 
@@ -38,27 +38,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack Application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }  
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },  
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   
   return (
     <div>
       <Header course={course} />
-      <Content content={
-        [
-          part1, part2, part3
-        ]} />
-      <Total total={part1.exercises + part2.exercises2 + part3.exercises3} />
+      <Content content={parts} />
+      <Total total={parts.map(part => part.exercises)} />
     </div>
   )
 }
