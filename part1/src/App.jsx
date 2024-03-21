6c3dc9c58e7f8848f4788262ad5150b99906d570
Daniel <danielnavatin@gmail.com>
2024-03-21 18:50:08 +0100

    Part 1.c until event handlers are functions

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Hello = ({name, age}) => {
   const bornYear = () => new Date().getFullYear() - age
 
@@ -14,14 +16,23 @@ const Hello = ({name, age}) => {
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const  [counter, setCounter]= useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
+      {counter}
+      <button onClick={increaseByOne}>
+      plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
     </div>
+    
   )
 }
 
