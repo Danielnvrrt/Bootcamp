266807b13062d5ed63c769df496e8978b94405d9
Daniel <danielnavatin@gmail.com>
2024-03-30 17:58:40 +0100

    Complete exercises 2.2 and 2.3

--- app/src/components/Content.jsx
+++ app/src/components/Content.jsx
@@ -1,11 +1,17 @@
 import Part from './Part'
+import Total from './Total'
 
 const Content = ({ parts }) => {
   return (
     <>
       {parts.map((part) => (
-        <Part part={part} />
+        <Part key={part.id} part={part} />
       ))}
+      <Total
+        total={parts
+          .map((part) => part.exercises)
+          .reduce((p1, p2) => p1 + p2)}
+      />
     </>
   )
 }
---
+++ app/src/components/Total.jsx
@@ -0,0 +1,5 @@
+const Total = ({total}) => {
+  return <b>total of {total} exercises</b>
+}
+
+export default Total
