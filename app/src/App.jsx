8b2a9d246ae93e502c39283e4f207cd5a0eb2121
Daniel <danielnavatin@gmail.com>
2024-03-30 17:22:21 +0100

    Create branch for part2

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,46 +1,15 @@
-import { useState } from "react"
+import Note from "./components/Note"
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = ({notes}) => {
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were probably born in {bornYear()}
-      </p>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
-  )
-}
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
-
-const App = () => {
-  const  [counter, setCounter]= useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-
-
-  return (
-    <div>
-      <Display counter={counter} />
-      <Button onSmash={increaseByOne}
-        text='plus'
-      />
-      <Button onSmash={setToZero}
-        text='zero'
-      />
-      <Button onSmash={decreaseByOne}
-        text='minus'
-      />
-    </div>
-    
   )
 }
 
