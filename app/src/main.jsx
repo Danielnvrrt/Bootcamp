7e197f9e986c3050a11c09932b12c7da4d0dd12c
danielnavamar <danielnavamar@gmail.com>
2024-08-05 17:10:59 +0200

    until exercise 6.10

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,19 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import React from "react"
-import { createStore, combineReducers } from 'redux'
+import { combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 import './index.css'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
