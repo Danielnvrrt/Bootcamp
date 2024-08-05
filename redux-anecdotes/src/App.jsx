2f28b9037269efb002253655eb8c1f4560975117
danielnavamar <danielnavamar@gmail.com>
2024-08-05 14:06:19 +0200

    complete exercise 6.5 and 6.6

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -9,21 +9,23 @@ const App = () => {
   const vote = (id) => {
     dispatch(voteAnecdote(id))
   }
-
+  
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+        )}
       <h2>create new</h2>
       <NewAnecdote />
     </div>
