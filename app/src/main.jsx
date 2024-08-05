f2d555aafe77ce4803595f5ac7972ac5549a401b
danielnavamar <danielnavamar@gmail.com>
2024-08-05 13:10:15 +0200

    Finalize part 6.a

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,16 @@
 import ReactDOM from 'react-dom/client'
+import React from "react"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './App'
+import noteReducer from './reducers/noteReducer'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const store = createStore(noteReducer)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
